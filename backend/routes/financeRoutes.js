de38783660610de12e9d9feb01fc3e6cd9258125
IT21311772 <it21311772@my.sliit.lk>
2023-09-09 12:26:37 +0530

    update

--- backend/routes/financeRoutes.js
+++ backend/routes/financeRoutes.js
@@ -1,48 +1,54 @@
 const express = require('express');
 const router = express.Router();
-const Trans = require('../models/financeModel');
+const Finance = require('../models/financeModel');
 
-// API route for Create Operation using POST
+// Create API route for Create method in CRUD Operations
 router.post("/add", (req, res) => {
-    Trans.create({
-        transaction: req.body.transaction,
-        type: req.body.type,
+    Finance.create({
         amount: req.body.amount,
+        type: req.body.type,
+        category: req.body.category,
         date: req.body.date,
+        description: req.body.description,
         reference: req.body.reference
     })
-    .then((doc) => console.log(doc))
-    .catch((err) => console.log(err));
+        .then((doc) => console.log(doc))
+        .catch((err) => console.log(err));
 });
 
-// API route for Read Operation using GET
-router.get("/docs", (req, res) => {
-    Trans.find()
+// Create API route for Read method in CRUD Operations
+router.get("/trans", (req, res) => {
+    Finance.find()
         .then((items) => res.json(items))
         .catch((err) => console.log(err));
 });
 
-// API route for Update Operation using PUT
+
+
+// Create API route for Delete method in CRUD Operations
+router.delete("/delete/:id", (req, res) => {
+    //create route for delete
+    Finance.findByIdAndDelete({ _id: req.params.id })
+      .then((doc) => console.log(doc))
+      .catch((err) => console.log(err));
+  });
+
+// Create API route for Update method in CRUD Operations
 router.put("/update/:id", (req, res) => {
-    Trans.findByIdAndUpdate(
+    Finance.findByIdAndUpdate (
         { _id: req.params.id},
         {
-            transaction: req.body.transaction,
-            type: req.body.type,
             amount: req.body.amount,
+            type: req.body.type,
+            category: req.body.category,
             date: req.body.date,
-            reference: req.body.reference
+            description: req.body.description,
+            reference: req.body.reference,
         }
     )
     .then((doc) => console.log(doc))
     .catch((err) => console.log(err));
-});
-
-// API route for Delete Operation using Delete
-router.delete("/delete/:id", (req, res) => {
-    Trans.findByIdAndDelete({ _id: req.params.id})
-        .then((doc) => console.log(doc))
-        .catch((err) => console.log(err));
+        
 });
 
 module.exports = router;
