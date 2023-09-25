b5d506770f4dbb87638e03859b1765aeef0c4126
IT21311772 <it21311772@my.sliit.lk>
2023-09-26 00:34:23 +0530

    navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import AddTransactions from './Components/addDetails';
 import Transactions from './Components/details';
 import FinanceReport from '../src/Components/FinanceReport';
+import About from './Components/About';
+import Contact from './Components/contactUs';
 
 function App() {
     return (
@@ -14,6 +16,8 @@ function App() {
                 <Route path='/fin/add' element={<AddTransactions />} />
                 <Route path='/fin/add/trans'element={<Transactions />} />
                 <Route path='/fin/report' element={<FinanceReport />} />
+                <Route path='/about' element={<About/>} />
+                <Route path='/contact' element={<Contact/>} />
             </Routes>
         </div>
     );
