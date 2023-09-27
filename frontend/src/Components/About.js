fabc83fd5dc32fe03ac9a6cff9fce7c5fc91cf36
IT21311772 <it21311772@my.sliit.lk>
2023-09-27 15:56:10 +0530

    about us

--- frontend/src/Components/About.js
+++ frontend/src/Components/About.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Header from './navBar';
+import Footer from './footer';
+import './contactUs.css';
+import Image from '../Images/image.jpeg';
 
 function About() {
   return (
     <div>
-      <h1>About Us</h1>
+      <Header />
+      <div className="about">
+        <div className="call">
+          <h2 className='head'>OUR MISSION</h2>
+          <p className='para'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus vitae animi qui sapiente iste vel explicabo soluta 
+            recusandae ad doloribus, placeat fugiat omnis possimus enim officia illum maxime blanditiis incidunt. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus vitae animi qui sapiente iste vel explicabo soluta 
+            recusandae ad doloribus, placeat fugiat omnis possimus enim officia illum maxime blanditiis incidunt?</p>
+        </div>
+        <div className="custom">
+          <img src={Image} alt="building" />
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
